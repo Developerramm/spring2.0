1a6f67d6885a2a97cdc6b3dd4dba7f8f55f9058d
ram kumar <rr697144@gmail.com>
2025-04-29 11:47:44 +0530

    spring beans ioc container config

--- springcorecontainer/src/main/java/springcorecontainer/Main.java
+++ springcorecontainer/src/main/java/springcorecontainer/Main.java
@@ -2,11 +2,18 @@ package springcorecontainer;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springcorecontainer.beans.User;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        ApplicationContext ap = new ClassPathXmlApplicationContext();
+        // System.out.println("Hello world!");
+        ApplicationContext ap = null;
+        ap = new ClassPathXmlApplicationContext("springcorecontainer/config/beanConfig.xml");
         System.out.println(ap);
+        // System.out.println(ap.getClass().getName());
+
+        User user = ap.getBean("user",User.class);
+        user.showUser();
+
     }
 }
---
+++ springcorecontainer/src/main/java/springcorecontainer/beans/User.java
@@ -0,0 +1,13 @@
+package springcorecontainer.beans;
+
+public class User {
+
+    public User(){
+        System.out.println("user constructor called");
+    }
+
+    public void showUser(){
+        System.out.println("Hello User this is show user method");
+    }
+
+}
