8d87c822257f6e2af66466f5230210345445380c
ram kumar <rr697144@gmail.com>
2025-05-03 11:40:47 +0530

    strategy design pattern

---
+++ strategydesignpattern/src/main/java/pattern/Main.java
@@ -0,0 +1,13 @@
+package pattern;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ApplicationContext ap = null;
+        ap = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println(ap);
+    }
+}
