b0c4fe585977253a5c27642f4e3b90360407877c
ram kumar <rr697144@gmail.com>
2025-05-12 09:23:40 +0530

    spring core layerd application setup

---
+++ springcore/layeredapp/src/main/java/springcore/Main.java
@@ -0,0 +1,13 @@
+package springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        ApplicationContext ap = new ClassPathXmlApplicationContext("config.xml");
+        System.out.println(ap);
+    }
+}
