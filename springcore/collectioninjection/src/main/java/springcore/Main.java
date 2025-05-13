b328d7a1e5c411294eb868a85cce2a74f7bfb072
ram kumar <rr697144@gmail.com>
2025-05-13 11:01:24 +0530

    list collection injection

--- springcore/collectioninjection/src/main/java/springcore/Main.java
+++ springcore/collectioninjection/src/main/java/springcore/Main.java
@@ -3,6 +3,7 @@ package springcore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import springcore.bean.Faculty;
 import springcore.bean.Student;
 
 public class Main {
@@ -15,6 +16,11 @@ public class Main {
         Student student = null;
         student = ap.getBean("student", Student.class);
         System.out.println(student.toString());
+        
+
+        Faculty faculty = null;
+        faculty = ap.getBean("faculty",Faculty.class);
+        System.out.println(faculty.toString());
 
         System.out.println(ap);
     }
