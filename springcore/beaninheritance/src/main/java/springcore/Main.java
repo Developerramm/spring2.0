85b75450816e10514a5cbbf2be72da2760afd1be
ram kumar <rr697144@gmail.com>
2025-05-14 09:58:15 +0530

    bean inheritance part 1

--- springcore/beaninheritance/src/main/java/springcore/Main.java
+++ springcore/beaninheritance/src/main/java/springcore/Main.java
@@ -3,12 +3,18 @@ package springcore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import springcore.beans.Car;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
         ApplicationContext ap = null;
         ap = new ClassPathXmlApplicationContext("config.xml");
+
+        Car car = null;
+        car = ap.getBean("car",Car.class);
+        System.out.println(car);
 
         System.out.println(ap);
     }
