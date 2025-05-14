ae5a632a9cdfdab4a78cb0f5b7a6d2cad37fa5df
ram kumar <rr697144@gmail.com>
2025-05-14 16:20:35 +0530

    collection merget in spring core

--- springcore/collectionmerge/src/main/java/springcore/Main.java
+++ springcore/collectionmerge/src/main/java/springcore/Main.java
@@ -3,11 +3,17 @@ package springcore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import springcore.beans.Student;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         ApplicationContext ap = null;
         ap = new ClassPathXmlApplicationContext("config.xml");
+
+        Student student = null;
+        student = ap.getBean("s1", Student.class);
+        System.out.println(student);
         System.out.println(ap);
     }
 }
---
+++ springcore/collectionmerge/src/main/java/springcore/beans/Student.java
@@ -0,0 +1,17 @@
+package springcore.beans;
+
+import java.util.Set;
+
+public class Student {
+    private Set<String> subjects;
+
+    public void setSubjects(Set<String> subjects) {
+        this.subjects = subjects;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [subjects=" + subjects + "]";
+    }
+
+}
