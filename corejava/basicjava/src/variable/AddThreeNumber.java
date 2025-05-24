49c5c97e4f898863e701ad600eb9838a256290aa
ram kumar <rr697144@gmail.com>
2025-05-24 12:12:22 +0530

    add three number

---
+++ corejava/basicjava/src/variable/AddThreeNumber.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class AddThreeNumber {
+    public static int addThree(int a, int b, int c){
+        return a + b + c;
+    }
+    public static void main(String[] args) {
+        int result = addThree(10,20,30);
+        System.out.println(result);
+    }
+}
