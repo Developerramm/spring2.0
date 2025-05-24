8f88dbe7e4c51bbe2c049f7db86ccc2f60211d06
ram kumar <rr697144@gmail.com>
2025-05-24 11:54:54 +0530

    data type in java

---
+++ corejava/basicjava/src/variable/Primitive.java
@@ -0,0 +1,36 @@
+package variable;
+
+public class Primitive {
+    public static void main(String[] args) {
+        System.out.println("Data type in java");
+        int x = 100;
+        System.out.println(x);
+        double y = 200.90;
+        System.out.println(y);
+
+        byte b1 = 23;
+        short s = 34;
+        int in = 2342;
+        long lo = 42343;
+        System.out.println(b1);
+        System.out.println(s);
+        System.out.println(in);
+        System.out.println(lo);
+
+        // floating point
+        System.out.println("Floating point data type");
+        float f1 = 432.43f;
+        double d1 = 43243.43244451312;
+        System.out.println(f1);
+        System.out.println(d1);
+
+        // char data type
+        char ch = 'A';
+        System.out.println(ch);
+
+
+        // boolean data type
+        boolean bool = true;
+        System.out.println(bool);
+    }
+}
