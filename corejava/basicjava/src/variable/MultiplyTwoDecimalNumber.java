96c279e83b9f1234fa43e35a5309b9419524af1f
ram kumar <rr697144@gmail.com>
2025-05-24 12:15:10 +0530

    multiply two decimal number

---
+++ corejava/basicjava/src/variable/MultiplyTwoDecimalNumber.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class MultiplyTwoDecimalNumber {
+    public static double multiplyTwoDecemal(float a, float b){
+        return a * b;
+    }
+    public static void main(String[] args) {
+        double result = multiplyTwoDecemal(23.43f,434.432f);
+        System.out.println(result);
+    }
+}
