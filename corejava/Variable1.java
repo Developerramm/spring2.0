c51049263d30b3f855717ba22d292776ce52b651
ram kumar <rr697144@gmail.com>
2025-05-24 11:12:25 +0530

    variable part 1

---
+++ corejava/Variable1.java
@@ -0,0 +1,11 @@
+class Variable1{
+public static void main(String[] args){
+	System.out.println(10);
+	int number = 100;
+	int number1 = 200;
+	System.out.println(number);
+	System.out.println(number1);
+
+}
+
+}
