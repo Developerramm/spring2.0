03d1061d299f2a0428454381b18e87de06f54695
ram kumar <rr697144@gmail.com>
2025-06-25 06:14:51 +0530

    sum of even number

--- DSA/basic/main.cpp
+++ DSA/basic/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int sumOfEvenNumber(int number)
+{
+    int sum = 0;
 
-    cout << "Hello world " << endl;
-    
+    for (int i = 0; i <= number; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter a number to get sum " << endl;
+    cin >> n;
+
+    int getSumOfEvenNumber = sumOfEvenNumber(n);
+
+    cout << getSumOfEvenNumber << endl;
+
     return 0;
 }
