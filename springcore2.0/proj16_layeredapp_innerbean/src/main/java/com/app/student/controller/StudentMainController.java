3272fbab5dee042b2b762dd33ae46631ec46c6e6
ram kumar <rr697144@gmail.com>
2025-07-07 11:26:31 +0530

    inner bean in layered app

---
+++ springcore2.0/proj16_layeredapp_innerbean/src/main/java/com/app/student/controller/StudentMainController.java
@@ -0,0 +1,37 @@
+package com.app.student.controller;
+
+import com.app.student.dto.StudentDto;
+import com.app.student.service.StudentService;
+import com.app.student.vo.StudentVo;
+
+public class StudentMainController {
+
+    private StudentService studentService;
+
+    public StudentMainController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    public void registerStudent(StudentVo vo) throws Exception {
+        // convert the vo to dto
+        StudentDto dto = null;
+
+        dto = new StudentDto();
+
+        dto.setId(Integer.parseInt(vo.getId()));
+        dto.setName(vo.getName());
+        dto.setEmail(vo.getEmail());
+        dto.setCourseName(vo.getCourseName());
+        dto.setObtainedMarks(Integer.parseInt(vo.getObtainedMarks()));
+
+        int i = studentService.registerNewStudent(dto);
+
+        if (i > 0) {
+            System.out.println("Record saved Successfully");
+        } else {
+            System.out.println("Record not save");
+        }
+
+    }
+
+}
