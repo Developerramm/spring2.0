ae09e48100125133fcada53f10879c372a07ac23
ram kumar <rr697144@gmail.com>
2025-07-07 12:00:23 +0530

    bean alias name in spring core

---
+++ springcore2.0/proj17_bean_alias_naming/src/main/java/com/app/Main.java
@@ -0,0 +1,29 @@
+package com.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.app.beans.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        ApplicationContext context = null;
+        context = new ClassPathXmlApplicationContext("config.xml");
+
+        Student student = null;
+
+        student = (Student) context.getBean("student");
+        System.out.println(student);
+
+        student = (Student) context.getBean("s1");
+        System.out.println(student);
+
+        student = (Student) context.getBean("10");
+        System.out.println(student);
+
+        System.out.println("-----------------------------------------");
+        System.out.println(context);
+    }
+}
