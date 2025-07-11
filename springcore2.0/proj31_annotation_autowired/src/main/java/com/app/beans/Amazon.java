1f1e80df116ff4ef893fe79362fbca4d4891839c
ram kumar <rr697144@gmail.com>
2025-07-11 11:23:56 +0530

    autowired at arbitrary method level

--- springcore2.0/proj31_annotation_autowired/src/main/java/com/app/beans/Amazon.java
+++ springcore2.0/proj31_annotation_autowired/src/main/java/com/app/beans/Amazon.java
@@ -1,11 +1,18 @@
 package com.app.beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Amazon {
 
-    @Autowired
+    // @Autowired
     private LogisticsService logisticsService;
+
+    @Autowired
+    @Qualifier("bluedart")
+    public void deliveryPartner(LogisticsService logisticsService) {
+        this.logisticsService = logisticsService;
+    }
 
     public void trackOrder(int orderId) {
         logisticsService.trackOrderLocation(orderId);
